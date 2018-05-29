50375b564b1467759485454b2f8c5c4d9b301d0d
Gerald Ruiz <gerald.ruiz@accenture.com>
2018-05-29 18:30:05 -0500

    second commit

---
+++ server.js
@@ -0,0 +1,56 @@
+console.log({ starting: true });
+
+import express from "express";
+
+const app = express();
+
+import graphqlHTTP from 'express-graphql';
+import { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLNonNull, GraphQLID } from 'graphql';
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQuery',
+  description: 'The root query',
+  fields: {
+    viewer: {
+      type: GraphQLString,
+      resolve() {
+        return 'viewer!';
+      }
+    }
+  }
+});
+
+let inMemoryStore = {};
+
+const RootMutation = new GraphQLObjectType({
+  name: 'RootMutation',
+  description: 'the root mutation',
+  fields: {
+    setNode: {
+      type: GraphQLString,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLID)
+        },
+        value: {
+          type: new GraphQLNonNull(GraphQLString),
+        }
+      },
+      resolve(source, args) {
+        inMemoryStore[args.key] = args.value;
+        return inMemoryStore[args.key];
+      }
+    }
+  }
+});
+
+const Schema = new GraphQLSchema({
+  query: RootQuery,
+  mutation: RootMutation,
+});
+
+app.use('/graphql', graphqlHTTP({schema: Schema, graphiql: true}));
+
+app.listen(3000, () => {
+  console.log({ running: true });
+});
